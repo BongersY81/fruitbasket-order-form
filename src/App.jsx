4eda19d6c2f0f4d54ebd6de45e5b9465ee9a0ff7
yvonnebongers <yvonnebongers@gmail.com>
2025-12-11 10:29:13 +0100

    state aangepast

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,76 @@
-
 import './App.css'
+import {useState} from "react";
 
 function App() {
 
-  return (
-    <>
-        <h1>Fruitmand bezorgservice</h1>
-    </>
-  )
+
+    const [strawBerry, setStrawberry] = useState(0);
+    const [banana, setBanana] = useState(0);
+    const [apple, setApple] = useState(0);
+    const [kiwi, setKiwi] = useState(0);
+
+    return (
+
+
+        <>
+
+            <h1>Fruitmand bezorgservice</h1>
+
+            <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
+                <span>Aardbeien</span>
+                <button
+                    onClick={() => setStrawberry(strawBerry - 1)}
+                    disabled={strawBerry === 0}
+                >
+
+                    -
+                </button>
+
+                <button onClick={() => setStrawberry(strawBerry + 1)}>
+                    +
+                </button>
+                <p>{strawBerry}</p>
+            </div>
+
+            <span>Bananen</span>
+            <button onClick={() => setBanana(banana - 1)}>
+
+                -
+            </button>
+
+            <button onClick={() => setBanana(banana + 1)}>
+                +
+            </button>
+
+            <p>{banana}</p>
+
+            <span>Appels</span>
+            <button onClick={() => setApple(apple - 1)}>
+
+                -
+            </button>
+
+            <button onClick={() => setApple(apple + 1)}>
+                +
+            </button>
+
+            <p>{apple}</p>
+
+            <span>Kiwi s</span>
+            <button onClick={() => setKiwi(kiwi - 1)}>
+
+                -
+            </button>
+
+            <button onClick={() => setKiwi(kiwi + 1)}>
+                +
+            </button>
+
+            <p>{kiwi}</p>
+
+            <button type="button">Reset</button>
+        </>
+    )
 }
 
 export default App
