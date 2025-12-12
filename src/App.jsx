138bf363b3447cd126d619bd3bbd9cb771e700c6
yvonnebongers <yvonnebongers@gmail.com>
2025-12-12 19:33:05 +0100

    opdracht 1 gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,27 @@ import {useState} from "react";
 function App() {
 
 
-    const [strawBerry, setStrawberry] = useState(0);
+    const [strawberry, setStrawberry] = useState(0);
     const [banana, setBanana] = useState(0);
     const [apple, setApple] = useState(0);
     const [kiwi, setKiwi] = useState(0);
+
+    // function resultFruitbasket() {
+    //     console.log(setStrawberry, setBanana, setApple,setKiwi())
+ const SelectedFruitTypes =
+     (strawberry > 0 ? 1 : 0) +
+     (banana > 0 ? 1 : 0) +
+     (apple > 0 ? 1 : 0) +
+     (kiwi > 0 ? 1 : 0) ;
+
+
+
+    function resetButton() {
+        setStrawberry(0);
+        setBanana(0);
+        setApple(0);
+        setKiwi(0);
+    }
 
     return (
 
@@ -16,25 +33,27 @@ function App() {
 
             <h1>Fruitmand bezorgservice</h1>
 
-            <div style={{display: "flex", alignItems: "center", gap: "10px"}}>
+
                 <span>Aardbeien</span>
                 <button
-                    onClick={() => setStrawberry(strawBerry - 1)}
-                    disabled={strawBerry === 0}
+                    onClick={() => setStrawberry(strawberry - 1)}
+                    disabled={strawberry === 0}
                 >
 
                     -
                 </button>
 
-                <button onClick={() => setStrawberry(strawBerry + 1)}>
+                <button onClick={() => setStrawberry(strawberry + 1)}>
                     +
                 </button>
-                <p>{strawBerry}</p>
-            </div>
+                <p>{strawberry}</p>
+
 
             <span>Bananen</span>
-            <button onClick={() => setBanana(banana - 1)}>
-
+            <button
+                onClick={() => setBanana(banana - 1)}
+                disabled={banana === 0}
+            >
                 -
             </button>
 
@@ -45,8 +64,9 @@ function App() {
             <p>{banana}</p>
 
             <span>Appels</span>
-            <button onClick={() => setApple(apple - 1)}>
-
+            <button onClick={() => setApple(apple - 1)}
+                    disabled={apple === 0}
+            >
                 -
             </button>
 
@@ -57,8 +77,9 @@ function App() {
             <p>{apple}</p>
 
             <span>Kiwi s</span>
-            <button onClick={() => setKiwi(kiwi - 1)}>
-
+            <button onClick={() => setKiwi(kiwi - 1)}
+                    disabled={kiwi === 0}
+            >
                 -
             </button>
 
@@ -68,7 +89,13 @@ function App() {
 
             <p>{kiwi}</p>
 
-            <button type="button">Reset</button>
+            <h2>Geselecteerde fruittypes: {SelectedFruitTypes}</h2>
+
+            <button onClick={resetButton}>
+                Reset
+            </button>
+
+
         </>
     )
 }
