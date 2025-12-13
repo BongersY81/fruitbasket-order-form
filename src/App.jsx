0548274e017ecf2ab2a677cdc455ba08d81c78d4
yvonnebongers <yvonnebongers@gmail.com>
2025-12-13 16:39:44 +0100

    opdracht 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,6 @@ function App() {
     const [banana, setBanana] = useState(0);
     const [apple, setApple] = useState(0);
     const [kiwi, setKiwi] = useState(0);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [age, setAge] = useState("");
-    const [postalCode, setPostalCode] = useState("");
-    // const [weeklyButton, setWeeklyButton] = useState("");
-    // const [dayAndEveningButton, setDayAndEveningButton] = useState("");
-    const [commentsField, setCommentsField] = useState("");
-    // const [termsAccepted, setTermsAccepted] = useState("");
-    // const [sendButton, setSendButton] = useState("");
 
 
     const SelectedFruitTypes =
@@ -24,6 +15,28 @@ function App() {
         (banana > 0 ? 1 : 0) +
         (apple > 0 ? 1 : 0) +
         (kiwi > 0 ? 1 : 0);
+
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [age, setAge] = useState("");
+    const [postalCode, setPostalCode] = useState("");
+    const [dayAndEveningButton, setDayAndEveningButton] = useState("");
+    const [commentsField, setCommentsField] = useState("");
+    const [termsAccepted, setTermsAccepted] = useState(false);
+    const [buttonOptions, setButtonOptions] = useState("");
+
+    console.log({
+        firstName,
+        lastName,
+        age,
+        postalCode,
+        dayAndEveningButton,
+        commentsField,
+        termsAccepted,
+        buttonOptions
+    })
+
+
 
 
     function resetButton() {
@@ -33,150 +46,206 @@ function App() {
         setKiwi(0);
     }
 
+
     return (
 
 
         <>
 
-        <h1>Fruitmand bezorgservice</h1>
+            <h1>Fruitmand bezorgservice</h1>
 
-        <div className="counter-row">
-            <span>Aardbeien</span>
-            <button
-                onClick={() => setStrawberry(strawberry - 1)}
-                disabled={strawberry === 0}
-            >
+            <div className="counter-row">
+                <span>Aardbeien</span>
+                <button
+                    onClick={() => setStrawberry(strawberry - 1)}
+                    disabled={strawberry === 0}
+                >
 
-                -
+                    -
+                </button>
+
+                <p>{strawberry}</p>
+
+                <button onClick={() => setStrawberry(strawberry + 1)}>
+                    +
+                </button>
+
+            </div>
+
+            <div className="counter-row">
+                <span>Bananen</span>
+                <button
+                    onClick={() => setBanana(banana - 1)}
+                    disabled={banana === 0}
+                >
+                    -
+
+                </button>
+
+                <p>{banana}</p>
+
+                <button onClick={() => setBanana(banana + 1)}>
+                    +
+                </button>
+
+
+            </div>
+
+            <div className="counter-row">
+                <span>Appels</span>
+                <button onClick={() => setApple(apple - 1)}
+                        disabled={apple === 0}
+                >
+                    -
+                </button>
+
+                <p>{apple}</p>
+
+                <button onClick={() => setApple(apple + 1)}>
+                    +
+                </button>
+
+
+            </div>
+
+            <div className="counter-row">
+                <span>Kiwi s</span>
+                <button onClick={() => setKiwi(kiwi - 1)}
+                        disabled={kiwi === 0}
+                >
+                    -
+                </button>
+
+                <p>{kiwi}</p>
+
+                <button onClick={() => setKiwi(kiwi + 1)}>
+                    +
+                </button>
+
+
+            </div>
+
+            <h2>Geselecteerde fruittypes: {SelectedFruitTypes}</h2>
+
+            <button onClick={resetButton}>
+                Reset
             </button>
 
-            <p>{strawberry}</p>
+            <form>
 
-            <button onClick={() => setStrawberry(strawberry + 1)}>
-                +
-            </button>
+                    <label htmlFor="form-firtsname">
+                        <p>Voornaam</p>
+                        <input
+                            type="text"
+                            id="form-firstname"
+                            name="firtsName"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                    </label>
 
-        </div>
+                    <label htmlFor="form-lastname">
+                        <p>Achternaam</p>
+                        <input type="text"
+                               id="form-lastname"
+                               name="lastName"
+                               value={lastName}
+                               onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="form-age">
+                        <p>Leeftijd</p>
+                        <input
+                            type="number"
+                            id="form-age"
+                            name="age"
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                    </label>
+                    <label htmlFor="form-postalcode">
+                        <p>Postcode</p>
+                        <input
+                            type="text"
+                            id="form-postalcode"
+                            name="postalCode"
+                            value={postalCode}
+                            onChange={(e) => setPostalCode(e.target.value)}
+                        />
+                    </label>
 
-        <div className="counter-row">
-            <span>Bananen</span>
-            <button
-                onClick={() => setBanana(banana - 1)}
-                disabled={banana === 0}
-            >
-                -
+                    <h2>Bezorgfrequentie</h2>
 
-            </button>
-
-            <p>{banana}</p>
-
-            <button onClick={() => setBanana(banana + 1)}>
-                +
-            </button>
-
-
-        </div>
-
-        <div className="counter-row">
-            <span>Appels</span>
-            <button onClick={() => setApple(apple - 1)}
-                    disabled={apple === 0}
-            >
-                -
-            </button>
-
-            <p>{apple}</p>
-
-            <button onClick={() => setApple(apple + 1)}>
-                +
-            </button>
-
-
-        </div>
-
-        <div className="counter-row">
-            <span>Kiwi s</span>
-            <button onClick={() => setKiwi(kiwi - 1)}
-                    disabled={kiwi === 0}
-            >
-                -
-            </button>
-
-            <p>{kiwi}</p>
-
-            <button onClick={() => setKiwi(kiwi + 1)}>
-                +
-            </button>
-
-
-        </div>
-
-        <h2>Geselecteerde fruittypes: {SelectedFruitTypes}</h2>
-
-        <button onClick={resetButton}>
-            Reset
-        </button>
-
-        <form>
-            <fieldset>
-                <label htmlFor="form-firtsname">
-                    Voornaam
-                    <input
-                        type="text"
-                        id="form-firstname"
-                        name="firtsName"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    />
-                </label>
-
-                <label htmlFor="form-lastname">
-                    Achternaam
-                    <input type="text"
-                           id="form-lastname"
-                           name="lastName"
-                           value={lastName}
-                           onChange={(e) => setLastName(e.target.value)}
-                    />
-                </label>
-                <label htmlFor="form-age">
-                    Leeftijd
-                    <input
-                        type="number"
-                        id="form-age"
-                        name="age"
-                        value={age}
-                        onChange={(e) => setAge(e.target.value)}
-                    />
-                </label>
-                <label htmlFor="form-postalcode">
-                       Postcode
-                <input
-                    type="text"
-                    id="form-postalcode"
-                    name="postalCode"
-                    value={postalCode}
-                    onChange={(e) => setPostalCode(e.target.value)}
-                />
-            </label>
-            <label htmlFor="form-commentsfield">
-                   opmerking
-                   <textarea
-                       id="form-commentsfield"
-                       name="commentsField"
-                       value={commentsField}
-                       onChange={(e) => setCommentsField(e.target.value)}
-                       />
-            </label>
-            <button type="submit">
-                Verzend
-            </button>
-        </fieldset>
-        </form>
+                    <label htmlFor="form-options">
+                        <select
+                            id="form-options"
+                            name="buttonOptions"
+                            value={buttonOptions}
+                            onChange={(e) => setButtonOptions(e.target.value)}
+                        >
+                            <option value=" "></option>
+                            <option value="iedere week">iedere week</option>
+                            <option value="om de week">om de week</option>
+                            <option value="iedere maand">iedere maand</option>
 
 
-</>
-)
+                        </select>
+
+                        <label htmlFor="form-day-and-evening-button">
+                            <input
+                                type="radio"
+                                name="dayAndEveningButton"
+                                value="overdag"
+                                checked={dayAndEveningButton === "overdag"}
+                                onChange={(e) => setDayAndEveningButton(e.target.value)}
+                            />
+                        </label>
+                            <p>Overdag</p>
+
+                        </label>
+
+                        <label htmlFor="form-day-and-evening-button">
+                            <input
+                                type="radio"
+                                name="dayAndEveningButton"
+                                value="s-avonds"
+                                checked={dayAndEveningButton === "s-avonds"}
+                                onChange={(e) => setDayAndEveningButton(e.target.value)}
+
+                            />
+                        </label>
+                            <p>`s Avonds</p>
+                            <label htmlFor="form-commentsfield">
+                                opmerking
+                                <textarea
+                                    id="form-commentsfield"
+                                    name="commentsField"
+                                    value={commentsField}
+                                    onChange={(e) => setCommentsField(e.target.value)}
+
+                                />
+                            </label>
+                            <label htmlFor="form-termsaccepted">
+                                Ik ga akoord met de voorwaarden
+                                <input
+                                    type="checkbox"
+                                    id="form-termsaccepted"
+                                    name="termsAccepted"
+                                    checked={termsAccepted}
+                                    onChange={() => setTermsAccepted(!termsAccepted)}
+                                />
+                            </label>
+
+                            <button type="submit">
+                                Verzend
+                            </button>
+
+
+            </form>
+
+
+        </>
+);
 }
 
 export default App
